eab5132a7146c4f0586db3c2dc951edbb1149ae4
Danilo Cestari <danilofcestari@gmail.com>
2018-11-06 23:50:06 +0100

    Add description

---
+++ README.md
@@ -0,0 +1,14 @@
+# This is still a work in progress
+
+For now, I would ask to please consider the technical aspects involved, like:
+
+* Server side rendering
+* Code Splitting
+* NodeJS / Express API
+* [React](https://reactjs.org/) best practices
+* [Redux](https://redux.js.org/) (static and dynamic imported)
+* [Redux-Observable](https://redux-observable.js.org/) middleware, powered by RxJs (with dynamic imported epics)
+* [Re-ducks](https://github.com/alexnm/re-ducks) modular architecture
+* CSS Grid
+* [Flow](https://flow.org/) type
+* [Sass](https://sass-lang.com) styles
--- src/components/MainLayout/index.js
+++ src/components/MainLayout/index.js
@@ -30,6 +30,45 @@ const MainLayout = (props: Props) => (
         showPage={props.showPage}
       />
     </div>
+    <div className="main-layout__info">
+      <h2>This is still a work in progress</h2>
+      For now, I would ask to please consider the technical aspects involved, like:
+      <ul>
+        <li>Server side rendering</li>
+        <li>Code Splitting</li>
+        <li>NodeJS / Express API</li>
+        <li>
+          <a href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">
+            React
+          </a> best practices
+        </li>
+        <li>
+          <a href="https://redux.js.org/" target="_blank" rel="noopener noreferrer">
+            Redux
+          </a> (static and dynamic imported)
+        </li>
+        <li>
+          <a href="https://redux-observable.js.org/" target="_blank" rel="noopener noreferrer">
+            Redux-Observable
+          </a> middleware, powered by RxJs (with dynamic imported epics)</li>
+        <li>
+          <a href="https://github.com/alexnm/re-ducks" target="_blank" rel="noopener noreferrer">
+            Re-ducks
+          </a> modular architecture
+        </li>
+        <li>CSS Grid</li>
+        <li>
+          <a href="https://flow.org/" target="_blank" rel="noopener noreferrer">
+            Flow
+          </a> type
+        </li>
+        <li>
+          <a href="https://sass-lang.com/" target="_blank" rel="noopener noreferrer">
+            Sass
+          </a> styles
+        </li>
+      </ul>
+    </div>
   </div>
 );
 
