b028847ba862db19d449a7ee018028d8e3b0abc4
Danilo Cestari <danilofcestari@gmail.com>
2019-06-27 00:10:33 +0200

    SSR test

--- testcafe/features/home-route.feature
+++ testcafe/features/home-route.feature
@@ -5,3 +5,8 @@ Feature: Home route feature
   Scenario: Accessing the Home route
     When I navigate to the home route
     Then I should see 10 book cards
+
+  Scenario: SSR home route
+    Given I'm logging server responses
+    When I navigate to the home route
+    Then some server response contains the Redux state
--- testcafe/steps/home-route.js
+++ testcafe/steps/home-route.js
@@ -1,10 +1,21 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const { When, Then } = require('cucumber');
+/* eslint-disable @typescript-eslint/no-var-requires, no-param-reassign */
+const { Given, When, Then } = require('cucumber');
+const { RequestLogger } = require('testcafe');
 const HomePage = require('../pages/home');
 
+const url = 'http://localhost:3001';
+
+Given("I'm logging server responses", async t => {
+  const logger = RequestLogger(new RegExp(url), {
+    logResponseBody: true,
+    stringifyResponseBody: true
+  });
+  t.ctx.requestLogger = logger;
+  await t.addRequestHooks(logger);
+});
+
 When(/^I navigate to the home route$/, async t => {
-  const page = new HomePage('http://localhost:3001', t);
-  // eslint-disable-next-line no-param-reassign
+  const page = new HomePage(url, t);
   t.ctx.page = page;
   await page.navigate();
 });
@@ -25,3 +36,22 @@ Then(
     await t.takeElementScreenshot(t.ctx.page.booksContainer);
   }
 );
+
+Then('some server response contains the Redux state', async t => {
+  await t.ctx.page.mainLayoutSelector();
+
+  /** @type { ReturnType<RequestLogger> } */
+  const logger = t.ctx.requestLogger;
+
+  await t
+    .expect(
+      logger.contains(
+        record =>
+          typeof record.response.body === 'string' &&
+          /window.__PRELOADED_STATE__\s*=\s*\{.+"home":\{.+\}.+\}/.test(
+            record.response.body
+          )
+      )
+    )
+    .ok();
+});
